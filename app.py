3d6c6eb0e6358bbdcd3cf636937eb75476594056
edogawa-aki <akinagaakina@gmail.com>
2026-03-20 17:13:38 +0900

    日本語対応

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+import streamlit.components.v1 as components
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
